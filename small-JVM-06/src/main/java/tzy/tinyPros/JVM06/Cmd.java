24ce203ed238e0e26abe9f84a871554f95b3c975
TPureZY <2036139108@qq.com>
2023-07-20 22:45:25 +0800

    :art: day 19 类和对象 3.1

--- small-JVM-06/src/main/java/tzy/tinyPros/JVM06/Cmd.java
+++ small-JVM-06/src/main/java/tzy/tinyPros/JVM06/Cmd.java
@@ -11,16 +11,16 @@ import java.util.Optional;
  * @since 2023/7/4 0:06
  **/
 public class Cmd {
-    @Parameter(names = {"-?","-h","-help"},description = "print help message",order = 3,arity = 0,help = true)
+    @Parameter(names = {"-?", "-h", "-help"}, description = "print help message", order = 3, arity = 0, help = true)
     boolean help = false;
 
-    @Parameter(names = {"-v","-version","-V"},description = "print jvm version",order = 2,arity = 0)
+    @Parameter(names = {"-v", "-version", "-V"}, description = "print jvm version", order = 2, arity = 0)
     boolean version = false;
 
-    @Parameter(names = {"-cp","-classpath"},description = "import classpath",order = 1,arity = 1)
+    @Parameter(names = {"-cp", "-classpath"}, description = "import classpath", order = 1, arity = 1)
     String classpath;
 
-    @Parameter(names = {"-Xjre"},description = "jre path(now using jre1.8 which has rj.jar)",order = 4)
+    @Parameter(names = {"-Xjre"}, description = "jre path(now using jre1.8 which has rj.jar)", order = 4)
     String jre;
 
     @Parameter(description = "main class and args")
@@ -28,27 +28,27 @@ public class Cmd {
 
     boolean ok;
 
-    Optional<String> getJre(){
+    public Optional<String> getJre() {
         return Optional.ofNullable(jre);
     }
 
-    Optional<String> getClasspath(){
+    public Optional<String> getClasspath() {
         return Optional.ofNullable(classpath);
     }
 
-    Optional<String> getMainClass(){
-        return mainClassAndArgs != null&&!mainClassAndArgs.isEmpty()?Optional.of(mainClassAndArgs.get(0)): Optional.empty();
+    public Optional<String> getMainClass() {
+        return mainClassAndArgs != null && !mainClassAndArgs.isEmpty() ? Optional.of(mainClassAndArgs.get(0)) : Optional.empty();
     }
 
-    Optional<List<String>> getAppArgs(){
-        return isLegal()?Optional.of(mainClassAndArgs.subList(1,mainClassAndArgs.size())): Optional.empty();
+    public Optional<List<String>> getAppArgs() {
+        return isLegal() ? Optional.of(mainClassAndArgs.subList(1, mainClassAndArgs.size())) : Optional.empty();
     }
 
-    boolean isLegal(){
-        return mainClassAndArgs != null&&mainClassAndArgs.size()>1;
+    public boolean isLegal() {
+        return mainClassAndArgs != null && mainClassAndArgs.size() > 1;
     }
 
-    static Cmd parse(String[] argv){
+    public static Cmd parse(String[] argv) {
         Cmd args = new Cmd();
         JCommander cmd = JCommander.newBuilder().addObject(args).build();
         // Parse and validate the command line parameters.
--- small-JVM-06/src/main/java/tzy/tinyPros/JVM06/Main.java
+++ small-JVM-06/src/main/java/tzy/tinyPros/JVM06/Main.java
@@ -1,7 +1,5 @@
 package tzy.tinyPros.JVM06;
 
-import tzy.tinyPros.JVM06.classfile.ClassFile;
-import tzy.tinyPros.JVM06.classfile.MemberInfo;
 import tzy.tinyPros.JVM06.classpath.Classpath;
 import tzy.tinyPros.JVM06.rtda.heap.ClassLoader;
 import tzy.tinyPros.JVM06.rtda.heap.methodarea.Class;
@@ -38,55 +36,4 @@ public class Main {
         new Interpreter(mainMethod);
     }
 
-    public static ClassFile loadClass(String className, Classpath cp) {
-        try {
-            // 读取字节码
-            byte[] classData = cp.readClass(className);
-            // 解析字节码
-            return new ClassFile(classData);
-        } catch (Exception e) {
-            System.out.println("Could not find or load main class: " + className);
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static void printClassStructure(ClassFile cf) {
-        System.out.printf("version: %d.%d\n", cf.getMajorVersion(), cf.getMinorVersion());
-        System.out.printf("constant_pool_count: %d\n", cf.getConstantPool().getSize());
-        System.out.printf("access_flags: 0x%x\n", cf.getAccessFlags());
-        System.out.printf("this_class_name: %s\n", cf.getClassName());
-        System.out.printf("super_class_name: %s\n", cf.getSuperClassName());
-        System.out.printf("interface_name_array: %s\n", Arrays.toString(cf.getInterfaceNames()));
-        System.out.printf("fields_count: %d\n", cf.getFields().length);
-        for (MemberInfo field : cf.getFields()) {
-            System.out.printf("%s \t\t %s\n", field.getName(), field.getDescriptor());
-        }
-        System.out.printf("methods_count: %d\n", cf.getMethods().length);
-        for (MemberInfo method : cf.getMethods()) {
-            System.out.printf("%s \t\t %s\n", method.getName(), method.getDescriptor());
-        }
-    }
-
-    public static void printBytes(byte[] classData) {
-        System.out.println("classData: ");
-        for (byte data : classData) {
-            // 使用16进制输出
-            System.out.print(String.format("%02x", data & 0xff) + " ");
-        }
-    }
-
-    public static void printUserArgs(Cmd cmd) {
-        System.out.printf("classpath:%s class:%s args:%s\n", cmd.getClasspath().orElse(null), cmd.getMainClass().orElse(null), cmd.getAppArgs().orElse(null));
-    }
-
-    public static MemberInfo getMainMethod(MemberInfo[] infos) {
-        for (MemberInfo info : infos) {
-            // 寻找main方法
-            if ("main".equals(info.getName()) && "([Ljava/lang/String;)V".equals(info.getDescriptor())) {
-                return info;
-            }
-        }
-        return null;
-    }
 }
